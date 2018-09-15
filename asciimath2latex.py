0102a28a1d315ef9d206e311de5bc113a7073ea3
Jerome FELLUS <jfellus@gmail.com>
2018-09-16 00:17:37 +0200

    expr parser

--- asciimath2latex.py
+++ asciimath2latex.py
@@ -10,10 +10,10 @@ def print_node(node, level):
         print_node(n, level+1)
 
 class Node(object):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, text=""):
         self.children = []
         self.parent = None
-        self.text = ""
+        self.text = text
         if parent:
             self.parent = parent
             parent.children.append(self)
@@ -22,9 +22,7 @@ class Node(object):
         text = text.strip()
         if text:
             if self.text:
-                n = Node(self.parent)
-                n.text = text
-                return n
+                return Node(self.parent, text)
             else:
                 self.text = text
         return self
@@ -51,9 +49,36 @@ def build_expressions_tree(x):
             curnode = curnode.parent
         x = x[i+1:]
 
+def process_binary_operators(tree):
+    i=0
+    while i<len(tree.children):
+        c = tree.children[i]
+        process_binary_operators(c)
+        if c.text != "/":
+            continue
+        iOp = c.text.find("/")
+        if iOp == -1:
+            continue
+        iStart = c.text.rfind(" ", 0, iOp)
+        if iStart == -1:
+            iStart = 0
+        iEnd = c.text.find(" ", iOp+1)
+        before = c.text[:iStart]
+        a = c.text[iStart+1:iOp]
+        b = c.text[iOp+1:iEnd]
+        after = c.text[iEnd+1:]
+        c.text = before
+        op = c.add_text_after("/")
+        L = Node(op, a)
+        R = Node(op, b)
+        op = op.add_text_after(after)
+
+
+
 def asciimath2latex(x):
     print(x)
     tree = build_expressions_tree(x)
+    tree = process_binary_operators(tree)
     tree.dump()
     return tree
 
