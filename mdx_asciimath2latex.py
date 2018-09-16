208216156c862809aedb85e76b32c852b1d428c9
Jerome FELLUS <jfellus@gmail.com>
2018-09-16 16:38:10 +0200

    added equation autoblock

--- mdx_asciimath2latex.py
+++ mdx_asciimath2latex.py
@@ -9,20 +9,23 @@ class ASCIIMath2LatexExtension(markdown.Extension):
 
     def extendMarkdown(self, md, md_globals):
         self.md = md
-
-        RE = r'\$([^\$]*)\$'
-
-        md.inlinePatterns.add('', ASCIIMath2LatexPattern(RE), '_begin')
+        md.postprocessors.add('asciimath-eq', ASCIIMath2LatexEquationPostProcessor(), "_begin")
+        md.postprocessors.add('asciimath', ASCIIMath2LatexPostProcessor(), "_begin")
 
     def reset(self):
         pass
 
-class ASCIIMath2LatexPattern(markdown.inlinepatterns.Pattern):
+class ASCIIMath2LatexPostProcessor(markdown.postprocessors.Postprocessor):
+    def run(self, x):
+        RE = r'\$([^\$]*)\$'
+        x = re.sub(RE, lambda y: "$"+asciimath2latex(y.group(1))+"$", x, flags=re.M)
+        return x
 
-    def handleMatch(self, m):
-        el = etree.Element("math")
-        el.text = asciimath2latex(m.group(0))
-        return el
+class ASCIIMath2LatexEquationPostProcessor(markdown.postprocessors.Postprocessor):
+    def run(self, x):
+        RE = r'^\s*\$([^\$]*)\$'
+        x = re.sub(RE, r"\\begin{equation}\n \1 \n \\end{equation}\n", x, flags=re.M)
+        return x
 
 def makeExtension(configs=None):
     return ASCIIMath2LatexExtension(configs=configs)
